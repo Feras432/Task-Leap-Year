0c89461cbfdcae3bdf41d35c67104ec011c00c33
Feras B A Y R Alshadad <83323@kfh.com>
2024-01-08 14:34:23 +0300

    Task Leap Year is Finished

---
+++ Task Leap Year/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner leapScanner = new Scanner(System.in);
+        System.out.println("Please enter a year: ");
+        int year = leapScanner.nextInt();
+        if (year % 4 == 0 && year % 100 != 0) {
+            System.out.println("The year you entered is a leap year. ");
+        } else if (year % 400 == 0) {
+
+            System.out.println("The year you entered is a leap year. ");
+        } else {
+            System.out.println("The year you entered is not a leap year. ");
+        }
+    }
+}
